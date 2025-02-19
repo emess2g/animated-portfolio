f74119a241bbeb855e987e73e65049c2b57ab1ec
emess2g <essuonemma84gmail.com>
2025-02-19 10:27:44 +0000

    import error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import  './app.scss';
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero'
 import Parallax from './components/parallax/Parallax';
-import Services from './services/Services';
+import Services from './components/services/Services';
 import Portfolio from './components/portfolio/Portfolio';
 import Contact from './components/contact/Contact';
 import Cursor from './components/cursor/Cursor';
